3506e854e2d611bb8826a04cde3cac678aaade11
Michael Mendy <montana@linux.com>
2023-11-28 07:56:23 -0800

    default export

---
+++ index.ts
@@ -0,0 +1,9 @@
+// Export a default export
+export { default } from './lib';
+
+// Export specific classes/functions
+export { MyClass } from './lib';
+
+// Export as a namespace
+import * as lib from './lib';
+export = lib;
